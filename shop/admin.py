f85295c41909ed392fc35df7bfc8c79b80a2c90a
HP <22201030@uap.edu.bd>
2025-04-28 19:51:06 +0600

    second commit

---
+++ shop/admin.py
@@ -0,0 +1,11 @@
+
+
+from django.contrib import admin
+from .models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'created_at')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at',)
+
+admin.site.register(Product, ProductAdmin)
---
+++ shop/models.py
@@ -0,0 +1,16 @@
+
+
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def _str_(self):
+        return self.name
+# Create your models here.
