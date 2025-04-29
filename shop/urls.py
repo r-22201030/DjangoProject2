50c16c16042d8cd74fef314ba3c1922eab2330cd
HP <22201030@uap.edu.bd>
2025-04-29 21:10:03 +0600

    6th

--- shop/urls.py
+++ shop/urls.py
@@ -18,11 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('ca/t', include('Category.urls')),
     path('login/', views.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 
 ]
